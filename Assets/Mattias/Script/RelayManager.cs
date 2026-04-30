14c94f3d74ee5f9c701c6c64331220896a7de741
Mattias Pothen Matthew <pothen.mattias@student.saedubai.com>
2026-04-30 22:06:05 +0400

    Added Host Migration so if the host leaves the client becomes the new host and can regenerate the code instead of the game closing fully

--- Assets/Mattias/Script/RelayManager.cs
+++ Assets/Mattias/Script/RelayManager.cs
@@ -5,6 +5,7 @@ using Unity.Services.Core;
 using Unity.Services.Authentication;
 using Unity.Services.Relay;
 using Unity.Services.Relay.Models;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 public class RelayManager : MonoBehaviour
@@ -25,9 +26,51 @@ public class RelayManager : MonoBehaviour
     {
         await UnityServices.InitializeAsync();
         await AuthenticationService.Instance.SignInAnonymouslyAsync();
+
+        NetworkManager.Singleton.OnClientDisconnectCallback += OnClientDisconnect;
     }
 
-    public async void CreateRelay()
+    private void OnDestroy()
+    {
+        if (NetworkManager.Singleton != null)
+            NetworkManager.Singleton.OnClientDisconnectCallback -= OnClientDisconnect;
+    }
+
+    //HOST MIGRATION
+    private void OnClientDisconnect(ulong clientId)
+    {
+        //If HOST disconnects
+        if (clientId == 0 && NetworkManager.Singleton.IsServer)
+        {
+            Debug.Log("HOST LEFT - starting migration...");
+
+            List<ulong> remainingClients = new List<ulong>(NetworkManager.Singleton.ConnectedClientsIds);
+
+            if (remainingClients.Count > 0)
+            {
+                ulong newHostId = remainingClients[0];
+
+                Debug.Log("NEW HOST WILL BE: " + newHostId);
+
+                PromoteNewHost(newHostId);
+            }
+        }
+    }
+
+    private async void PromoteNewHost(ulong newHostId)
+    {
+        //Shutdown current session
+        NetworkManager.Singleton.Shutdown();
+
+        //Small delay to ensure cleanup
+        await Task.Delay(1000);
+
+        //New host creates a NEW relay session
+        await CreateRelay();
+    }
+
+    //RELAY
+    public async Task CreateRelay()
     {
         Allocation allocation = await RelayService.Instance.CreateAllocationAsync(2);
 
@@ -35,7 +78,7 @@ public class RelayManager : MonoBehaviour
 
         showCode = true;
 
-        Debug.Log("JOIN CODE: " + currentJoinCode);
+        Debug.Log("NEW JOIN CODE: " + currentJoinCode);
 
         transport.SetHostRelayData(
             allocation.RelayServer.IpV4,
